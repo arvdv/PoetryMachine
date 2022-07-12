dc2c90740f9ce2246a6ccc8bc7ebf36f6ec8d96f
jr725 <jr725@exeter.ac.uk>
2022-07-12 15:11:53 +0100

    Added app and utils

---
+++ app.py
@@ -0,0 +1,17 @@
+from utils import *
+import streamlit as st
+
+
+st.title("The Poetry Machine")
+
+c1, c2 = st.columns(2)
+
+with c1:
+    text_in = st.text_input("Poetry Subject")
+with c2:
+    number_words = st.slider("Poem Length", 10, 80)
+    go_button = st.button("Compose")
+
+
+if go_button:
+    st.write(generate_poem(text_in, number_words))
